d90b3382501cc309ed942e474a8a1555146b0243
legoheld <michael.heimann@lernetz.ch>
2016-06-22 10:21:38 +0200

    Added first version of Dockerfile and entrypoint.sh.

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# run client
+eval $(ssh-agent -s)
+
+# inject private key
+ssh-add <(echo "$SSH_PRIVATE_KEY")
+
+# ignore host checkig as it needs terminal
+mkdir -p ~/.ssh
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
